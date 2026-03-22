08790f57d44bf0c61299b9133c3de006edead24d
Nachiket Amlekar <nachiketamlekar@Nachikets-MacBook-Pro.local>
2026-03-22 13:21:27 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ connectDB();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: (process.env.CLIENT_URL || 'http://localhost:5173').replace(/\/+$/, ''),
   credentials: true,
 }));
 
